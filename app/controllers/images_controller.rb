2357f0fb9beb0b4848ee83fb28eb6b1f3931ff4b
arpethel <arpethel@gmail.com>
2019-07-14 10:00:32 -0400

    managed image routs

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,35 +1,39 @@
 class ImagesController < ApplicationController
 
     get '/images' do
-    if logged_in?
-      @images = Image.all
-      erb :'images/images'
-    else
-      redirect '/login'
-    end
-  end
-
-  post '/save_image' do
-    if logged_in?
-      if params[:title] !=""
-        @image = Image.new(title: params[:title][:file][:filename])
-        @image.user = current_user
-        @image.save
-
-        file = params[:file][:tempfile]
-
-        File.open("./public/#{@image}", 'wb') do |i|
-        i.write(file.read)
+        if logged_in?
+        @images = Image.all
+        erb :'images/images'
+        else
+        redirect '/login'
         end
-
-        redirect "/images/#{@image.id}"
-      else
-        redirect '/images/upload_image'
-      end
-    else
-      redirect '/login'
     end
-  end
+
+    get '/images/upload_image' do
+        erb :"images/upload_image"
+    end
+
+    post '/save_image' do
+        if logged_in?
+        if params[:title] !=""
+            @image = Image.new(title: params[:title][:file][:filename])
+            @image.user = current_user
+            @image.save
+
+            file = params[:file][:tempfile]
+
+            File.open("./public/#{@image}", 'wb') do |i|
+            i.write(file.read)
+            end
+
+            redirect "/images/show_image"
+        else
+            redirect '/images/upload_image'
+        end
+        else
+        redirect '/login'
+        end
+    end
 
     get '/images/:id' do
         if logged_in?
