31c375f10a689bfa06fa4fa4f343b021c59e5f85
arpethel <arpethel@gmail.com>
2019-07-14 11:08:09 -0400

    rolled back schema | only posts and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190714114211) do
-
-  create_table "images", force: :cascade do |t|
-    t.string  "title"
-    t.integer "post_id"
-  end
+ActiveRecord::Schema.define(version: 20190714062900) do
 
   create_table "posts", force: :cascade do |t|
     t.string  "caption"
