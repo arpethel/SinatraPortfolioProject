0e18226787be9abe9fede519421a4d5b1791a05b
arpethel <arpethel@gmail.com>
2019-07-14 09:42:53 -0400

    built out images routes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -10,18 +10,34 @@ class ImagesController < ApplicationController
   end
 
   post '/save_image' do
-    @filename = params[:file][:filename]
-    file = params[:file][:tempfile]
+    if logged_in?
+      if params[:title] !=""
+        @image = Image.new(title: params[:title][:file][:filename])
+        @image.user = current_user
+        @image.save
 
-    File.open("./public/#{@filename}", 'wb') do |f|
-      f.write(file.read)
+        file = params[:file][:tempfile]
+
+        File.open("./public/#{@image}", 'wb') do |i|
+        i.write(file.read)
+        end
+
+        redirect "/images/#{@image.id}"
+      else
+        redirect '/images/upload_image'
+      end
+    else
+      redirect '/login'
     end
-    
-    erb :"posts/show_image"
   end
 
+    get '/images/:id' do
+        if logged_in?
+            @image = Image.find_by_id(params[:id])
+            erb :"images/show_image"
+        else
+            redirect to '/login'
+        end
+    end
 
-
-
-  
 end
