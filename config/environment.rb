872ac5fe91cf383f2ad38284ae33f069a122049e
arpethel <arpethel@gmail.com>
2019-07-17 11:41:50 -0400

    deleted files associated with heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,40 +1,11 @@
 ENV['SINATRA_ENV'] ||= "development"
 
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
-
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "postgresql",
-#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
-
-# require_all 'app'
-
-# configure :production do
-#    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-#    ActiveRecord::Base.establish_connection(
-#      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#      :host     => db.host,
-#      :username => db.user,
-#      :password => db.password,
-#      :database => db.path[1..-1],
-#      :encoding => 'utf8'
-#      )
-
-#      require_all 'app'
-# end
-
-
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-Bundler.require(:default, :development)
-
-db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
 ActiveRecord::Base.establish_connection(
-  :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :database => db.path[1..-1]
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
 require_all 'app'
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20190714153931) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "posts", force: :cascade do |t|
     t.string  "caption"
     t.integer "user_id"
